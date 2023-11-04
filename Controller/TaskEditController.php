2b52b829e997e5f08c35d5e01b1ded499b906d35
Sefqon1 <severin.klee@gmail.com>
2023-11-04 15:35:52 +0700

    Refactored TaskEditController

--- Controller/TaskEditController.php
+++ Controller/TaskEditController.php
@@ -4,26 +4,30 @@ class TaskEditController
 {
     public function index($connection,$id): void
     {
-        $databaseConnection = new DatabaseConnection();
-        $connection = $databaseConnection->getConnection();
         $taskRepository = new TaskRepository($connection);
         $task = $taskRepository->getById('parenttask', $id);
-
         if (isset($_POST['submit'])) {
-            $taskName = $_POST['taskName'];
-            $taskDescription = $_POST['taskDescription'];
-            $dueDate = new DateTime($_POST['dueDate']);
-            $updatedTask = new ParentTask($id, $taskName, $taskDescription, $dueDate, (bool)null);
-            if($taskRepository->update('parenttask', $updatedTask))
-            {
-                header("Location: index.php?page=task/" . $id);
-            } else {
-                echo 'Didnt work lol';
-            }
+            $this->updateTask($taskRepository, $id);
         }
 
 
         require 'View/TaskEdit.php';
+    }
+
+    public function updateTask($taskRepository, $id) {
+
+        $taskName = $_POST['taskName'];
+        $taskDescription = $_POST['taskDescription'];
+        $dueDate = new DateTime($_POST['dueDate']);
+
+        $updatedTask = new ParentTask($id, $taskName, $taskDescription, $dueDate, (bool)null);
+
+        if($taskRepository->update('parenttask', $updatedTask))
+        {
+            header("Location: index.php?page=task/" . $id);
+        } else {
+            throw new Exception('Editing Task Failed');
+        }
     }
 
 }
