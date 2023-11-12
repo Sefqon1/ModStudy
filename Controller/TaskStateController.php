8231a7cc05fe1631de6a3c8a4eb6f1a8afa2470b
Sefqon1 <severin.klee@gmail.com>
2023-11-12 18:31:01 +0700

    Version 1.0
    First feature complete and running version. May need some refactoring, but is working so far.

--- Controller/TaskStateController.php
+++ Controller/TaskStateController.php
@@ -16,17 +16,9 @@ class TaskStateController
                     break;
 
                 case 'parenttask':
-                    if (isset($_POST['confirm'])) {
-                        $repository = new TaskRepository($connection);
-                        $repository->updateState($table, $id);
-                        header("Location: index.php?page=/");
-                    } else {
-                        echo '<form method="post">
-                        <p>Are you sure you want to mark this task as complete? This is not reversible!</p>
-                        <input type="submit" name="confirm" value="Yes">
-                        <a href="index.php?page=/">No</a>
-                        </form>';
-                    }
+                    $repository = new TaskRepository($connection);
+                    $repository->updateState($table, $id);
+                    header("Location: index.php?page=/");
                     break;
             }
         }
